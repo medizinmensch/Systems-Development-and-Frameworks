68c7fb35fcb07774da59187f9d3bb93d35cba185
d064467 <oskar.sailer@sap.com>
2019-11-19 13:57:46 +0100

    start to improve code according to PR

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,46 +1,56 @@
 import { mount } from '@vue/test-utils';
 import app from '../src/App.vue';
+import list from '../src/components/list.vue'
 
 describe('App.vue', () => {
 
-  const wrapper = mount(app);
+  let wrapper = mount(app);
+  beforeEach(() => {
+    wrapper = mount(app);
+  });
+
   it('renders the todo app', () =>{
     expect(wrapper.contains('div')).toBe(true);
   });
 
-  //for some unforsaken reason, this one refuses to work.
   describe('when the toggleEditModeb function is called', () => {
-    const editButton = wrapper.findAll('button').at(2);
-    editButton.trigger('click');
     it('opens an input field in a listitem component', () => {
+        const editButton = wrapper.findAll('button').at(2);
+        editButton.trigger('click');
         expect(wrapper.html()).toContain('input');
     });
   });
 
   describe('when the deleteEntryb function is called', () => {
-    wrapper.vm.deleteEntryb(0);
+    let listItemWrapper;
+    beforeEach(() => {
+      listItemWrapper = wrapper.find(list);
+      wrapper.find(list).find('#buttonDelete').trigger('click')
+    });
     it('decreases the amount of items in the list', () => {
-      expect(wrapper.vm.items).toHaveLength(3);
+      expect((listItemWrapper.props().items.length)).toBe(3);
     });
     it('removes the item with the specified id', () => {
-      expect(wrapper.vm.items).not.toContain('"id": 0');
+      expect(listItemWrapper.props().items).not.toContain('"id": 0');
     });
   });
 
-
-
   describe('when a the addEntry function is called', () => {
+    let listItemWrapper;
+    beforeEach(() => {
+      listItemWrapper = wrapper.find(list);
+      //console.log(wrapper.find('addEntry'));
+      wrapper.vm.addEntry();
+    });
 
-    const wrapper2 = mount(app);
-    wrapper2.vm.addEntry();
     it('increases the amount of items in the list', () => {
-      expect(wrapper2.vm.items).toHaveLength(5); //previous test has decreased it
+      expect(listItemWrapper.props().items).toHaveLength(5); //previous test has decreased it
     });
     it('increments the maxId property', () => {
-      expect(wrapper2.vm.maxId).toBe(4); //is not decreased when entries are removed
+      expect(wrapper.vm.maxId).toBe(4); //is not decreased when entries are removed
     });
-    it('opens an input field in the  listitem component', () => {
-        expect(wrapper2.html()).toContain('input');
+    it('opens an input field in the listitem component', () => {
+        expect(wrapper.html()).toContain('input');
     });
   });
 });
