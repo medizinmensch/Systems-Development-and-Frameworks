253fc80c357230c1a904a0a7bc3f418d41cb62c4
d064467 <oskar.sailer@sap.com>
2019-11-21 00:08:45 +0100

    add basic backend apollo tests for resolvers

--- backend/test/resolver.spec.js
+++ backend/test/resolver.spec.js
@@ -1,9 +1,16 @@
-const { ApolloServer, gql } = require('apollo-server');
-const server = require("../server")
-
-
+const {ApolloServer, gql} = require('apollo-server-express');
 const {createTestClient} = require('apollo-server-testing');
+const typeDefs = require('../schema.js');
+const resolvers = require('../resolvers.js');
 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const {query, mutate} = createTestClient(server);
+
+// could not outsource these queries/mutations...
 const ALL_ITEMS_QUERY = gql`
     query itemsQuery {
         items {
@@ -14,9 +21,43 @@ const ALL_ITEMS_QUERY = gql`
     }
 `;
 
-const {query, mutate} = createTestClient(server);
+const CREATE_ENTRY = gql`
+    mutation createEntry($text: String!) {
+        createEntry(text: $text) {
+            id,
+            text,
+            editMode
+        }
+    }
+`;
 
-it("behaves in a specified way", async (done) => {
-    await expect(query({query: ALL_ITEMS_QUERY
-    })).resolves.toMatchObject("bla")    
+const DELETE_ENTRY = gql`
+    mutation DeleteEntry($id: String!) {
+        deleteEntry(id: $id)
+    }
+`;
+
+// todo's: add beforeEach's, add more/better test cases
+describe('Queries', () => {
+    it("has start todo items", () => {
+        query({query: ALL_ITEMS_QUERY}).then((data) => {
+            expect(data.data.items).toHaveLength(4);
+        });
+    });
+});
+
+describe('Mutations', () => {
+    const exampleText = "example text";
+    it("creates entry", () => {
+        mutate({mutation: CREATE_ENTRY, variables: {text: exampleText}})
+            .then((data) => {
+                expect(data.data.createEntry.id).anything
+            });
+    });
+    it("deletes entry", () => {
+        mutate({mutation: DELETE_ENTRY, variables: {id: "3"}})
+            .then((data) => {
+                expect(data.data.deleteEntry).toBe(true)
+            });
+    });
 });
