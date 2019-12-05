93a97bf3b671cb4b15adce0a0d458ab0587fc449
d064467 <oskar.sailer@sap.com>
2019-12-05 13:39:44 +0100

    adjust db script to also insert user name for each todo

--- backend/helper/seed_db.js
+++ backend/helper/seed_db.js
@@ -1,19 +1,25 @@
 const {getDriver} = require('../neo4j/neo4j.js');
 const uuidv1 = require('uuid/v1');
+const users = require('../users.js');
 
 const exampleTodoTexts = ["Take out the trash", "Get A+ in SDAF", "git commit -m 'this'", "git diff --cached or --staged"];
-console.log();
+const exampleUsers = [];
+users.forEach(user => {
+    exampleUsers.push(user.name);
+});
 
 async function createTestData(amount) {
     await console.log(`Creating test data. ${amount} Todo's will be created.`);
     for (let i = 0; i<amount; i++) {
         const session = driver.session();
-        const exampleText = exampleTodoTexts[Math.floor(Math.random()*(exampleTodoTexts.length-1))];
+        const exampleText = exampleTodoTexts[Math.floor(Math.random()*(exampleTodoTexts.length))];
+        const randomUser = exampleUsers[Math.floor(Math.random()*(exampleUsers.length))];
         const resultPromise = await session.run(
-            'CREATE (a:Todo {id: $id, text: $text}) RETURN a',
+            'CREATE (a:Todo {id: $id, text: $text, user: $user}) RETURN a',
             {
                 id: uuidv1(),
-                text: exampleText
+                text: exampleText,
+                user: randomUser
             }
         );
         await console.log("Created Todo: " + resultPromise.records[0].get(0));
